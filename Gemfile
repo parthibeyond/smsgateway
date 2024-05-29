3236d1cf6245b3bf0f0b6f81ad91f15273e5c845
Parthi Beyond <parthi@beyondfinity.com>
2024-05-29 22:28:41 +0530

    chore: code cleanup

--- Gemfile
+++ Gemfile
@@ -28,16 +28,17 @@ gem "tzinfo-data", platforms: %i[ windows jruby ]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
+gem "figaro"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin Ajax possible
 # gem "rack-cors"
-
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
+  gem 'rspec-rails', '~> 6.1.0'
 end
 
 group :development do
--- app/models/sms.rb
+++ app/models/sms.rb
@@ -6,29 +6,32 @@ class Sms
 
   validates_each :from, :to, :text do |record, attr, value|
     record.errors.add attr, "is missing" if value.blank?
-    record.errors.add attr, "is invalid" if ([:from, :to].include?(attr) && out_of_limit(value&.size.to_i))
-    record.errors.add attr, "is invalid" if ([:text].include?(attr) && out_of_limit(value&.size.to_i))
+    record.errors.add attr, "is invalid" if ([:from, :to].include?(attr) && out_of_limit(value&.size.to_i, 6, 16))
+    record.errors.add attr, "is invalid" if (attr.eql?(:text) && out_of_limit(value&.size.to_i, 1, 120))
   end
 
-  def self.out_of_limit(limit)
-    (limit < 6) && (limit > 16)
+  def self.out_of_limit(limit, min, max)
+    (limit < min) || (limit > max)
   end
 
   def validate_inbound_condition
     validate
-    phone = account.phone_number.find_by(number: to)
-    errors.add(:to, "parameter not found") if phone.nil?
+    errors.add(:to, "parameter not found") unless phone_exists?(to)
   end
 
   def validate_outbound_condition
     validate
-    phone = account.phone_number.find_by(number: from)
-    errors.add(:from, "parameter not found") if phone.nil?
-    errors.add(:base, "sms from #{from} to #{to} blocked by STOP request") if $redis.get("#{from}_#{to}")
-    errors.add(:base, "limit reached for from #{from}") if $redis.get("#{from}_count").to_i > 50
+    errors.add(:from, "parameter not found") unless phone_exists?(from)
+    errors.add(:base, "sms from #{from} to #{to} blocked by STOP request") if stop_command_exists?(from, to)
+    errors.add(:base, "limit reached for from #{from}") if exceeded_rate_limit?(from)
+  end
+
+  def phone_exists?(number)
+    account.phone_number.find_by(number: number)
   end
 
   def stop_command_exists?(from, to)
+    $redis.get("#{from}_#{to}")
   end
 
   def increment_sms_count(from)
@@ -37,5 +40,6 @@ class Sms
   end
 
   def exceeded_rate_limit?(from)
+    $redis.get("#{from}_count").to_i > 50
   end
 end
